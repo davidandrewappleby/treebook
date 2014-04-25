b4963c25df19beb5b84e1c122a10c08176c179c2
David Appleby <davidandrewappleby@gmail.com>
2014-04-25 17:26:29 +0100

    Added and refactored flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  #attr_accessible :first_name, :last_name, :profile_name,
-  #				  :email, :password
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
